0db96972bebd58822cc27f36b6f37ae16bc300b6
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-02-20 20:29:44 -0600

    5

---
+++ m_5.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+# Longest Palindromic Substring
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        s = [c for c in s]
+        retVal = [s[0]]
+        stack = []
+        for i in range(1, len(s)):
+            
+            # Checking for types aa, abba, cbba
+            j = i
+            stack = s[:i][:] #Converting string to list of chars
+            rec = []
+            while len(stack) > 0 and j < len(s) and stack.pop() == s[j]:
+                rec.append(s[j])
+                j = j + 1
+            if len(rec) > 0:
+                if len(rec[::-1]+ rec) > len(retVal):
+                    retVal = rec[::-1]+ rec
+                
+            # Checking for types aba, ccacc
+            j = i
+            stack = s[:i][:]
+            rec = []
+            first = stack.pop()
+            while len(stack) > 0 and j < len(s) and stack.pop() == s[j]:
+                rec.append(s[j])
+                j = j + 1
+            if len(rec) > 0:
+                if len(rec[::-1]+ [first] + rec) > len(retVal): 
+                    retVal = rec[::-1]+ [first] + rec
+                
+        return "".join(retVal)
+            
