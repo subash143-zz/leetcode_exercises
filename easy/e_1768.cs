505d55dad4f0813fc8ddead5fd13dc33915927b7
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-04-24 11:53:21 -0500

    Merge Strings Alternatively

--- easy/e_1768.cs
+++ easy/e_1768.cs
@@ -3,32 +3,31 @@
 
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-        var retVal = "";
+        var retVal = new StringBuilder();
         
         int i = 0;
         int j = 0;
-        bool first = true;
         while(i < word1.Length && j < word2.Length){
-            retVal += word1[i];
-            retVal += word2[j];
+            retVal.Append(word1[i]);
+            retVal.Append(word2[j]);
             i++;
             j++;
         }
         
         if(i < word1.Length){
             while(i < word1.Length){
-                retVal += word1[i];
+                retVal.Append(word1[i]);
                 i++; 
             }
         }
         else if(j < word2.Length){
             while(j < word2.Length){
-                retVal += word2[j];
+                retVal.Append(word2[j]);
                 j++;
             }
         }
         
-        return retVal;
+        return retVal.ToString();
         
     }
 }
